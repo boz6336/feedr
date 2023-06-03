8b16b3027606061059db9ce9b4ffed5f5b397902
David <david@taiko.xyz>
2023-06-04 02:19:43 +0800

    feat(eth): update `DeliverBodies` to use `CalcWithdrawalsRootTaiko` (#87)
    * feat(eth): update `DeliverBodies` to use `CalcWithdrawalsRootTaiko`
    
    * chore: update workflow

--- eth/downloader/queue.go
+++ eth/downloader/queue.go
@@ -794,9 +794,16 @@ func (q *queue) DeliverBodies(id string, txLists [][]*types.Transaction, txListH
 				return errInvalidBody
 			}
 			// CHANGE(taiko): change the empty withdrawalsRoot hash to EmptyCodeHash
-			if withdrawalListHashes[index] == types.EmptyRootHash && os.Getenv("TAIKO_TEST") == "" {
-				withdrawalListHashes[index] = types.EmptyCodeHash
+			if withdrawalListHashes[index] == types.EmptyRootHash {
+				if os.Getenv("TAIKO_TEST") == "" {
+					withdrawalListHashes[index] = common.HexToHash("0x569e75fc77c1a856f6daaf9e69d8a9566ca34aa47f9133711ce065a571af0cfd")
+				}
+			} else {
+				if os.Getenv("TAIKO_TEST") == "" {
+					withdrawalListHashes[index] = types.CalcWithdrawalsRootTaiko(withdrawalLists[index])
+				}
 			}
+
 			if withdrawalListHashes[index] != *header.WithdrawalsHash {
 				return errInvalidBody
 			}
